ece4faaa1f33bcc7cd486103430f33b86b57f344
Oleg Kornienko <kornienko340@gmail.com>
2019-07-09 13:07:32 +0300

    Initial Commit

---
+++ App in the air Stories/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  App in the air Stories
+//
+//  Created by Oleg Kornienko on 09.07.2019.
+//  Copyright © 2019 Oleg Kornienko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
